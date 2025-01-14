109a4854ec372d5ef2766f4123c82adba7b3453a
Alex <sasha.shustov.05@mail.ru>
2025-01-14 10:45:21 +0300

    initial commit

---
+++ src/App.tsx
@@ -0,0 +1,9 @@
+import Listing from "./components/Listing/Listing";
+import data from "./data/data.json";
+import "./App.css";
+
+function App() {
+  return <Listing items={data} />;
+}
+
+export default App;
---
+++ src/components/Listing/Listing.tsx
@@ -0,0 +1,18 @@
+import React from "react";
+import ListItem, { Item } from "./ListItem/ListItem";
+
+type listingProps = {
+  items: any;
+};
+
+const Listing: React.FC<listingProps> = ({ items }) => {
+  return (
+    <ul className="item-list">
+      {items.map((item: Item) => (
+        <ListItem key={item.listing_id} item={item} />
+      ))}
+    </ul>
+  );
+};
+
+export default Listing;
