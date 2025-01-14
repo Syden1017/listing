19912a9a5d8a5af488476dcf5f438962d739f7d3
Alex <sasha.shustov.05@mail.ru>
2025-01-14 18:56:02 +0300

    little changes

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,11 @@
 import Listing from "./components/Listing/Listing";
-import data from "./data/data.json";
+import etsy from "./data/etsy.json";
 import "./App.css";
 
 function App() {
+  const jsonString = JSON.stringify(etsy);
+  const data = JSON.parse(jsonString);
+
   return <Listing items={data} />;
 }
 
